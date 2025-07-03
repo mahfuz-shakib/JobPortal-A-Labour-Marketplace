08c8ef284e8ddde0a5d4d411d6a8b48d764d9d97
mahfuz-shakib <mahfuzshakib301@gmail.com>
2025-07-03 23:41:14 +0600

    cors setup done

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 // app.use(cors());
  app.use(cors(
   {
-    origin:['htttp://localhost:5000','https://labour-marketplace.vercel.app/']
+    origin:['htttp://localhost:5000','https://labour-marketplace.netlify.app/']
   }
  ));
 app.use(express.json({ limit: '5mb' }));
