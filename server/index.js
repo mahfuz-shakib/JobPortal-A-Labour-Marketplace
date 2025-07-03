40a9a2980466ce295b201c5effe517dfb926f07b
mahfuz-shakib <mahfuzshakib301@gmail.com>
2025-07-03 23:36:30 +0600

    cors setup done

--- server/index.js
+++ server/index.js
@@ -10,7 +10,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+// app.use(cors());
+ app.use(cors(
+  {
+    origin:['htttp://localhost:5000','https://labour-marketplace.vercel.app/']
+  }
+ ));
 app.use(express.json({ limit: '5mb' }));
 
 
